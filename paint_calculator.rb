8349ee0350eebafc43ccf3db605d42ea30257670
chuck1193 <rooswalls@gmail.com>
2022-02-19 10:50:06 -0800

    Updated paint calculator

--- paint_calculator.rb
+++ paint_calculator.rb
@@ -1,12 +1,20 @@
 def paint_calculator
-  puts "What is the length of the room in feet? "
-  length = gets.chomp.to_i
+  number = false
 
-  puts "What is the width of the room in feet? "
-  width = gets.chomp.to_i
+  until number == true
+    puts "What is the length of the room in feet? "
+    length = gets.chomp.to_i
 
-  area = length * width 
+    puts "What is the width of the room in feet? "
+    width = gets.chomp.to_i
 
+    area = length * width 
+    if width == 0 || length == 0 
+      puts "you need to enter an actual number. "
+    else
+      number = true
+    end
+  end
   if area <= 350 
     puts "You will need to purchase 1 gallon of paint to cover #{area} square feet."
   else
@@ -15,8 +23,7 @@ def paint_calculator
 
     if leftover_paint > 0
       gallons += 1
-      puts area
-      puts gallons
+      puts "You will need to purchase #{gallons} gallons of paint to cover #{area} square feet."
     end
   end
 end
