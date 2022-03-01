9bd0bf74d0261943bb1c308c7291812359da79b8
chuck1193 <rooswalls@gmail.com>
2022-02-28 18:35:35 -0800

    Updated self checkout

--- self_checkout.rb
+++ self_checkout.rb
@@ -1,36 +1,39 @@
 def self_checkout
-  puts "Enter the price of the item: "
-  price1 = gets.chomp.to_f
+  done = false
+  until done == true 
+    puts "Enter the price of the item: "
+    price1 = gets.chomp.to_f
 
-  puts "Enter the quantity of item: "
-  quantity1 = gets.chomp.to_f
+    puts "Enter the quantity of item: "
+    quantity1 = gets.chomp.to_f
 
-  puts "Enter the price of the item: "
-  price2 = gets.chomp.to_f  
+    puts "Enter the price of the item: "
+    price2 = gets.chomp.to_f  
 
-  puts "Enter the quantity of item: "
-  quantity2 = gets.chomp.to_f
+    puts "Enter the quantity of item: "
+    quantity2 = gets.chomp.to_f
 
-  puts "Enter the price of the item: "
-  price3 = gets.chomp.to_f
+    puts "Enter the price of the item: "
+    price3 = gets.chomp.to_f
 
-  puts "Enter the quantity of item: "
-  quantity3 = gets.chomp.to_f
-
-  puts "Enter the price of the item: "
-  price4 = gets.chomp.to_f
-
-  puts "Enter the quantity of item: "
-  quantity4 = gets.chomp.to_f
-
-  subtotal = price1 + price2 + price3 + price4
-  tax = subtotal * 0.055
-  total = subtotal + tax
+    puts "Enter the quantity of item: "
+    quantity3 = gets.chomp.to_f
 
 
-  puts "$#{subtotal}"
-  puts "$#{tax}"
-  puts "$#{total}"
+    if price1 == 0.0 || quantity1 == 0.0 || price2 == 0.0 || quantity2 == 0.0 || price3 == 0.0 || quantity3 == 0.0
+      puts "You have to enter something so I can properly calculate the total."
+      redo
+    else
+      subtotal = (price1 * quantity1) + (price2 * quantity2) + (price3 * quantity3)
+      tax = subtotal * 0.055
+      total = subtotal + tax
+      done = true 
+    end
+  end
+
+  puts "Subtotal: $#{subtotal}"
+  puts "Tax: $#{tax}"
+  puts "Total: $#{total.round(2)}"
 end
 
 self_checkout
