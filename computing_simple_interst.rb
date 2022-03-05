4f3d1f792a9fd9bfd44a91317a663f7afcc835e0
chuck1193 <rooswalls@gmail.com>
2022-03-05 11:42:32 -0800

    Updated computing simple interest to check if the input is correct

--- computing_simple_interst.rb
+++ computing_simple_interst.rb
@@ -1,18 +1,27 @@
 def computing_simple_interest
-  puts "Enter the principal: "
-  principal = gets.chomp.to_f
+  correct_info == false
+  until correct_info == true
+    puts "Enter the principal: "
+    principal = gets.chomp.to_f
 
-  puts "Enter the rate of interest: "
-  r = gets.chomp.to_f
+    puts "Enter the rate of interest: "
+    r = gets.chomp.to_f
 
-  puts "Enter the number of years: "
-  time = gets.chomp.to_f
+    puts "Enter the number of years: "
+    time = gets.chomp.to_f
 
-  rate = r / 100.0
+    if principal == 0.0 || r == 0.0 || time == 0.0
+      puts "What are you doing? I need actual input to be able to accurrately tell you the simple interest."
+      redo
+    else
+      rate = r / 100.0
 
-  amount = principal * (1 + (rate * time))
+      amount = principal * (1 + (rate * time))
 
-  puts "After #{time.round} years at #{r}%, the investment will be worth $#{amount.round}."
+      puts "After #{time.round} years at #{r}%, the investment will be worth $#{amount.round}."
+      correct_info == true
+    end
+  end
 end
 
 computing_simple_interest
