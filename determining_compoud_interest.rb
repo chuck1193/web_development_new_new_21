bc581d76fc63f2e56952eb5a400aab76de739575
chuck1193 <rooswalls@gmail.com>
2022-03-11 11:36:11 -0800

    Updated determining compound interest

--- determining_compoud_interest.rb
+++ determining_compoud_interest.rb
@@ -11,10 +11,15 @@ def compound_interest
   puts "What is the number of times the interest is compounded per year? "
   n = gets.chomp.to_f
 
-  rate = r / 100
+  if p == 0.0 || r == 0.0 || t == 0.0 || n == 0.0
+    puts "I need you to put in actual information so that I can give you actual answers."
+  else
 
-  a = p * (1 + (rate / n) ** (n * t))
-  puts a 
+    rate = r / 100
+
+    a = p * (1 + (rate / n) ** (n * t))
+    puts a 
+  end
   # puts "$#{p} invested at #{r}%/ for #{t} years compunded #{n} times per year is $#{a}"  
 end
 
